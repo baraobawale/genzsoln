0acdbe015030192674c0a3668599261bad03dfa7
Babar Ganesh <ganesh.babar@dab.com>
2019-09-12 11:57:03 +0530

    Tags added to run particular feature file

--- src/test/java/com/bnpp/runner/JunitRunner.java
+++ src/test/java/com/bnpp/runner/JunitRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true,features = "src/test/java/com/bnpp/features/", glue={"com/bnpp/steps/"})
+@CucumberOptions(monochrome = true,features = "src/test/java/com/bnpp/features/", glue={"com/bnpp/steps/"},tags="@RTARTA")
 
 public class JunitRunner {
 
