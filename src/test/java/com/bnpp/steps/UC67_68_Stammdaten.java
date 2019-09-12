68a0599026a3f8af8b791bcbb64bff0766108ec8
Babar Ganesh <ganesh.babar@dab.com>
2019-09-12 11:58:57 +0530

    Step definition as per changes in feature file

--- src/test/java/com/bnpp/steps/UC67_68_Stammdaten.java
+++ src/test/java/com/bnpp/steps/UC67_68_Stammdaten.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.HashMap;
 import org.json.simple.parser.ParseException;
+import org.junit.Assert;
 
 import com.bnpp.library.CommonActions;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
+
 
 public class UC67_68_Stammdaten {
 
@@ -21,53 +22,7 @@ public class UC67_68_Stammdaten {
 
 	HashMap<String, String> hmap = new HashMap<String, String>();
 
-	/**
-	 * 
-	 * @param AccountName
-	 *            Description Login with Depot account.
-	 * @throws InterruptedException
-	 * @throws MalformedURLException
-	 * @throws FileNotFoundException
-	 * @throws IllegalArgumentException
-	 */
-	@When("^User open an application$")
-	public void User_logs_in_with_Depot_account()
-			throws InterruptedException, MalformedURLException, IllegalArgumentException, FileNotFoundException {
-		commonActions.launchBrowser();
-		commonActions.click("logInButton");
-	}
-
-//	@Then("Verify ErrorMesssages are displayed")
-//	public void verify_error_messsages_are_displayed(DataTable dataTable) {
-//		try {
-//			List<List<Object>> list = dataTable.asLists(String.class);
-//			if ((list.get(2).toString()).contains(commonActions.getText("Branchie_errorMessage"))) {
-//				commonActions.logPassStatus("Branchie message displayed successfully");
-//			} else {
-//				commonActions.logAssert_Fail("Branchie error message display: Error");
-//			}
-//			if ((list.get(3).toString()).contains(commonActions.getText("plz_errorMessage"))) {
-//				commonActions.logPassStatus("PLZ message displayed successfully");
-//			} else {
-//				commonActions.logAssert_Fail("PLZ error message display: Error");
-//			}
-//			if ((list.get(4).toString()).contains(commonActions.getText("StraBe_errorMessage"))) {
-//				commonActions.logPassStatus("Strasse message displayed successfully");
-//			} else {
-//				commonActions.logAssert_Fail("Strasse error message display: Error");
-//			}
-//			if ((list.get(5).toString()).contains(commonActions.getText("Ort_errorMessage"))) {
-//				commonActions.logPassStatus("Ort message displayed successfully");
-//			} else {
-//				commonActions.logAssert_Fail("Ort error message display: Error");
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			commonActions.logAssert_Fail("Error message validation fail");
-//		}
-//	}
-
-	@Then("^Verify \"(.*?)\" are displayed$")
+	@Then("^Verify displayed \"(.*?)\"$")
 	public void verify_error_messsages_are_displayed(String ErrorMesssages)
 			throws FileNotFoundException, IOException, ParseException {
 
@@ -85,7 +40,7 @@ public class UC67_68_Stammdaten {
 			e.printStackTrace();
 			Assert.fail();
 		}
-}
+	}
 
 	@Then("^verify changed data shown on bestaetigung page$")
 	public void verify_changed_data_shown_on_bestaetigung_page() {
